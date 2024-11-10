ff4ec6349140caacabee76788b1ef1e1c18db6c3
Jathn <jonathan.ramstedt@gmail.com>
2024-11-10 18:31:45 +0200

    Docs introduction, impressed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+/**
+ * # hello_rust
+ * 
+ * *Function that returns a greeting message.*
+ * 
+ * ## Parameters
+ * 
+ * - `Author`: A string slice (`&str`) that represents the name of the author.
+ * 
+ * ## Returns
+ * 
+ * A `String` containing a a greeting, with a specification who sent it.
+ * 
+ * ## Example
+ * 
+ * HelloRust("Dan") would return the following:
+ * 
+ * Hello Rust.
+ * ~ Dan
+ */
+fn hello_rust(author: &str) -> String {
+    let result: String = format!("Hello Rust.\n~ {}", author);
+    return result;
+}
+
+/**
+ * # main
+ * 
+ * Prints the following string:<br>
+ * Hello Rust.<br>
+ * ~ Jathn<br>
+ * to the command line.
+ * 
+ */
 fn main() {
-    println!("Hello, world!");
+    let greeting: String = hello_rust("Jathn");
+    println!("{}", greeting);
 }
