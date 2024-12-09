35c715cc213ed775c43649c1826427658dd88c80
Jathn <jonathan.ramstedt@gmail.com>
2024-12-09 19:34:33 +0200

    Docs etc. on dynamictypes

--- src/customtypes/mod.rs
+++ src/customtypes/mod.rs
@@ -2,93 +2,93 @@ use std::ops::*;
 
 /**
  * File: Int.rs
- * 
+ *
  * Holder of the Int enum, to be used for integer representation with dynamic handling of the size of the integer.
- * 
+ *
  */
 pub enum Int {
-    I8Bit(i8),
-    I16Bit(i16),
-    I32Bit(i32),
-    I64Bit(i64),
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
 }
 
 impl Int {
     /**
      * # init
-     * 
+     *
      * Initializes the Int enum with the value provided.
-     * 
+     *
      * ## Parameters
-     * 
+     *
      * - `value`: An integer value that will be used to initialize the Int enum.
-     * 
+     *
      * ## Returns
-     * 
+     *
      * An Int enum with the value provided.
-     * 
+     *
      */
     pub fn init(value: i64) -> Int {
         match value {
-            value if value <= i8::MAX as i64 && value >= i8::MIN as i64 => Int::I8Bit(value as i8),
-            value if value <= i16::MAX as i64 && value >= i16::MIN as i64 => Int::I16Bit(value as i16),
-            value if value <= i32::MAX as i64 && value >= i32::MIN as i64 => Int::I32Bit(value as i32),
-            _ => Int::I64Bit(value),
+            value if value <= i8::MAX as i64 && value >= i8::MIN as i64 => Int::I8(value as i8),
+            value if value <= i16::MAX as i64 && value >= i16::MIN as i64 => Int::I16(value as i16),
+            value if value <= i32::MAX as i64 && value >= i32::MIN as i64 => Int::I32(value as i32),
+            _ => Int::I64(value),
         }
     }
 
     /**
      * # get_value
-     * 
+     *
      * Returns the value of the Int enum.
-     * 
+     *
      * ## Returns
-     * 
+     *
      * The value of the Int enum.
-     * 
+     *
      */
     pub fn get_value(&self) -> i64 {
         match *self {
-            Int::I8Bit(val) => val as i64,
-            Int::I16Bit(val) => val as i64,
-            Int::I32Bit(val) => val as i64,
-            Int::I64Bit(val) => val,
+            Int::I8(val) => val as i64,
+            Int::I16(val) => val as i64,
+            Int::I32(val) => val as i64,
+            Int::I64(val) => val,
         }
     }
 
     /**
      * # _get_type
-     * 
+     *
      * Returns the type of the Int enum.
-     * 
+     *
      * ## Returns
-     * 
+     *
      * A string that represents the type of the Int enum.
-     * 
+     *
      */
     pub fn _get_type(&self) -> String {
         match *self {
-            Int::I8Bit(_) => "i8".to_string(),
-            Int::I16Bit(_) => "i16".to_string(),
-            Int::I32Bit(_) => "i32".to_string(),
-            Int::I64Bit(_) => "i64".to_string(),
+            Int::I8(_) => "i8".to_string(),
+            Int::I16(_) => "i16".to_string(),
+            Int::I32(_) => "i32".to_string(),
+            Int::I64(_) => "i64".to_string(),
         }
     }
 
     /**
      * # op
-     * 
+     *
      * Performs an operation on two Int enums.
-     * 
+     *
      * ## Parameters
-     * 
+     *
      * - `other`: A reference to an Int enum that will be used in the operation.
      * - `op`: A function that will be used to perform the operation.
-     * 
+     *
      * ## Returns
-     * 
+     *
      * An Int enum with the result of the operation.
-     * 
+     *
      */
     fn op<F>(&self, other: &Int, op: F) -> Int
     where
@@ -105,7 +105,7 @@ impl Int {
 
 /**
  * Implementations of the Add, Sub, Mul, and Div traits for the Int enum.
- * 
+ *
  */
 impl Mul for Int {
     type Output = Int;
@@ -133,7 +133,7 @@ impl Add for Int {
 
 impl Sub for Int {
     type Output = Int;
-    
+
     fn sub(self, other: Int) -> Int {
         self.op(&other, |a: i64, b: i64| a - b)
     }
--- src/main.rs
+++ src/main.rs
@@ -5,52 +5,56 @@ mod customtypes;
 
 /**
  * # hello_rust
- * 
+ *
  * *Function that returns a greeting message.*
- * 
+ *
  * ## Parameters
- * 
+ *
  * - `Author`: A string slice (`&str`) that represents the name of the author.
- * 
+ *
  * ## Returns
- * 
+ *
  * A `String` containing a a greeting, with a specification who sent it.
- * 
+ *
  * ## Example
- * 
+ *
  * HelloRust("Dan") would return the following:
- * 
+ *
  * Hello Rust.
  * ~ Dan
  */
 fn hello_rust(author: &str) -> String {
     let result: String = format!("Hello Rust.\n~ {}", author);
-    return result;
+    result
 }
 
 /**
  * # test_int_type
- * 
+ *
  * *Function that tests the Int custom type.*
- * 
+ *
  */
 fn test_int_type() {
     let int1: customtypes::Int = customtypes::Int::init(10);
     println!("int1 = {} of type: {}", int1.get_value(), int1._get_type());
     let int2: customtypes::Int = customtypes::Int::init(50000);
     println!("int2 = {} of type: {}", int2.get_value(), int2._get_type());
-    let int3: customtypes::Int = int1 + int2;
-    println!("int1 + int2 = {} of type: {}", int3.get_value(), int3._get_type());
+    let int3: customtypes::Int = int1 + int2 + customtypes::Int::init(5000000000000000000);
+    println!(
+        "int1 + int2 + very large int = {} of type: {}",
+        int3.get_value(),
+        int3._get_type()
+    );
 }
 
 /**
  * # main
- * 
+ *
  * Prints the following string:<br>
  * Hello Rust.<br>
  * ~ Jathn<br>
  * to the command line.
- * 
+ *
  */
 fn main() {
     let greeting: String = hello_rust("Jathn");
